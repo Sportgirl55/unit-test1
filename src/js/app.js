6ded76ef9b0b69c4c5cfc12294df8bf8a106c1e8
Elena Slyusareva <sportgirl55@bk.ru>
2020-12-23 14:07:33 +0500

    Mistakes was fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,4 @@
 
-// const heroes = { name: 'Маг', health: 10 };
-
 export default function getHealth(obj) {
   if (obj.health > 50) {
     return ('healthy');
@@ -8,10 +6,5 @@ export default function getHealth(obj) {
   if (obj.health >= 15 && obj.health <= 50) {
     return ('wounded');
   }
-  if (obj.health < 15) {
-    return ('critical');
-  }
+  return ('critical');
 }
-
-
-
